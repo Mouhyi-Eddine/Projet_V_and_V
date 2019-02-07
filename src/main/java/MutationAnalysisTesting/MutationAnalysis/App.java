ce00c963cabfdac35ffe0fe27204728e1c888546
mjj94 <m.jammal94@gmail.com>
2019-02-07 18:06:46 +0100

    recuperer les class dans une maniere generiques

--- src/main/java/MutationAnalysisTesting/MutationAnalysis/App.java
+++ src/main/java/MutationAnalysisTesting/MutationAnalysis/App.java
@@ -1,39 +1,24 @@
 package MutationAnalysisTesting.MutationAnalysis;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.MethodVisitor;
-import static org.objectweb.asm.Opcodes.ASM5;
-
-import javassist.ClassPool;
-import javassist.CodeConverter;
-import javassist.CtClass;
-import javassist.CtMethod;
-import javassist.NotFoundException;
-import javassist.bytecode.CodeAttribute;
-import javassist.bytecode.Opcode;
-
-public class App extends ClassVisitor{
-    public App(int api) {
-        super(api);
-    }
-
-    @Override
-    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-        System.out.println(String.format("%s %s", name, desc));
-        return super.visitMethod(access, name, desc, signature, exceptions);
-    }
-
-    public static void main(String... args) throws IOException {
-
-        // Intended to be executed from the root of the project
-
-        FileInputStream file = new FileInputStream("/home/mj/vv-workspace/code-manipulation/manipulation-target/target/classes/m2/vv/tutorial/Functions.class");
-        ClassReader reader = new ClassReader(file);
-        reader.accept(new App(ASM5), 0);
-
-    }
+public class App {
+//	public static void main(String[] args) throws NotFoundException {
+//		ClassPool pool = ClassPool.getDefault();
+//		pool.appendClassPath("/home/mj/vv-workspace/testing-tutorial/target/classes/");
+//		CtClass functions = pool.get("m2.vv.tutorial.algorithms.ReversePolishExpressionEvaluator");
+//
+//		System.out.println("Declared methods:");
+//		System.out.println("---");
+//
+//		for (CtMethod method : functions.getDeclaredMethods()) {
+//			CodeAttribute attribute =  method.getMethodInfo().getCodeAttribute();
+//			byte[] code = attribute.getCode();
+//			for(int i =0; i < code.length; i++) {
+//				if(code[i] == Opcode.DMUL) {
+//					code[i] = Opcode.DADD;
+//				}
+//			}
+//			System.out.println(method.getName());
+//		}
+//	}
 }
--- src/main/java/MutationAnalysisTesting/MutationAnalysis/InstructionModifier.java
+++ src/main/java/MutationAnalysisTesting/MutationAnalysis/InstructionModifier.java
@@ -1,59 +1,100 @@
 package MutationAnalysisTesting.MutationAnalysis;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 import org.objectweb.asm.*;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 import static org.objectweb.asm.Opcodes.ASM5;
 
 public class InstructionModifier extends ClassVisitor {
 
+	public InstructionModifier(int api, ClassVisitor classVisitor) {
+		super(api, classVisitor);
+	}
 
-    public InstructionModifier(int api, ClassVisitor classVisitor) {
-        super(api, classVisitor);
-    }
+	@Override
+	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+//		MethodVisitor visitor = new MethodVisitor(api) {
+//			@Override
+//			public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+//				// TODO Auto-generated method stub
+//				System.out.println("desc: " + desc);
+//				return super.visitAnnotation(desc, visible);
+//			}
+//		};
+		MethodVisitor visitor = super.visitMethod(access, name, desc, signature, exceptions);;
+		if (name.equals("twicee")) { // Too specific uh? It is better to have a parameter
+			return new StartByAddModifier(api, visitor);
+		}
 
-    @Override
-    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+		return visitor;
+	}
 
-        MethodVisitor visitor = super.visitMethod(access, name, desc, signature, exceptions);;
+	static class StartByAddModifier extends MethodVisitor {
 
-        if(name.equals("twice")) { // Too specific uh? It is better to have a parameter
-            return new StartByAddModifier(api, visitor);
-        }
-        return visitor;
-    }
+		public StartByAddModifier(int api, MethodVisitor methodVisitor) {
+			super(api, methodVisitor);
+		}
 
-    static class StartByAddModifier extends MethodVisitor {
+		@Override
+		public void visitInsn(int opcode) {
+			if (opcode == Opcodes.DMUL) {
+				opcode = Opcodes.DADD;
+			} else if (opcode == Opcodes.LAND) {
+				opcode = Opcodes.LOR;
+			}
+			super.visitInsn(opcode);
+		}
+	}
 
-        public StartByAddModifier(int api, MethodVisitor methodVisitor) {
-            super(api, methodVisitor);
-        }
+	public static ArrayList<File> getFiles(String target) {
+		File folder = new File(target);
+		File[] listOfFiles = folder.listFiles();
+		ArrayList<File> files = new ArrayList<File>();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			File file = listOfFiles[i];
+			if (file.isFile()) {
+				System.out.println("File " + file.getName());
+				files.add(file);
+			} else if (file.isDirectory()) {
+				System.out.println("Directory " + file.getName());
+				files.addAll(getFiles(target + file.getName() + "/"));
+			}
+		}
+		return files;
+	}
 
-        @Override
-        public void visitInsn(int opcode) {
-            if (opcode == Opcodes.DMUL) {
-                opcode = Opcodes.DADD;
-            }else if (opcode == Opcodes.LAND) {
-            	opcode = Opcodes.LOR;
-            }
-            super.visitInsn(opcode);
-        }
-    }
+	public static void main(String... args) throws Exception {
+		ArrayList<File> files = getFiles("/home/mj/vv-workspace/manipulation-target/target/classes/");
+		final String target = "/home/mj/vv-workspace/manipulation-target/target/classes/";
+//		final String outputFile = "/home/mj/vv-workspace/manipulation-target/target/classes/Test.class";
+		for (File f : files) {
+			FileInputStream file = new FileInputStream(f);
+			ClassReader reader = new ClassReader(file);
+			ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
+			Result result = JUnitCore.runClasses(reader.getClass());
+			System.out.println(reader.getClassName() + "  ");
+			for(Constructor c: reader.getClass().getConstructors()) {
+				System.out.println(c.getName());
+			}
+			for(Failure fail: result.getFailures()) {
+				System.out.println(fail.toString());
+			}
+			System.out.println(result.wasSuccessful());
+			reader.accept(new InstructionModifier(ASM5, writer), ClassReader.EXPAND_FRAMES);
 
-    public static void main(String... args) throws Exception {
-
-        final String target = "/home/mj/vv-workspace/code-manipulation/manipulation-target/target/classes/m2/vv/tutorial/Functions.class";
-
-        FileInputStream file = new FileInputStream(target);
-        ClassReader reader = new ClassReader(file);
-
-        ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
-
-        reader.accept(new InstructionModifier(ASM5, writer), ClassReader.EXPAND_FRAMES);
-
-        FileOutputStream output = new FileOutputStream(target);
-        output.write(writer.toByteArray());
-
-    }
+			FileOutputStream output = new FileOutputStream(f);
+			output.write(writer.toByteArray());
+			output.close();
+		}
+	}
 }
-
