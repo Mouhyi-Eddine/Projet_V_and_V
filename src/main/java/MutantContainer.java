d787516b49d9b9374b27d53197c3a4e7514b68c7
mjj94 <m.jammal94@gmail.com>
2019-02-09 13:57:33 +0100

    creation de la class pour init la mutation

---
+++ src/main/java/MutantContainer.java
@@ -0,0 +1,22 @@
+
+class MutantContainer {
+	private String mutantClass;
+	private String methodName;
+
+	public String getMutatedClass() {
+		return mutantClass;
+	}
+
+	public void setMutatedClass(String mutatedClass) {
+		this.mutantClass = mutatedClass;
+	}
+
+	public String getMutationMethod() {
+		return methodName;
+	}
+
+	public void setMutationMethod(String methodName) {
+		this.methodName = methodName;
+	}
+
+}
