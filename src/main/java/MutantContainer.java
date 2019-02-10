9d5cea8380804cd60fe8653b68e51d3dc51c3348
mjj94 <m.jammal94@gmail.com>
2019-02-10 18:15:02 +0100

    ajouter typeMutation

--- src/main/java/MutantContainer.java
+++ src/main/java/MutantContainer.java
@@ -2,6 +2,7 @@
 class MutantContainer {
 	private String mutantClass;
 	private String methodName;
+	private String mutantType;
 
 	public String getMutatedClass() {
 		return mutantClass;
@@ -17,6 +18,14 @@ class MutantContainer {
 
 	public void setMutationMethod(String methodName) {
 		this.methodName = methodName;
+	}
+
+	public String getMutantType() {
+		return mutantType;
+	}
+
+	public void setMutantType(String mutantType) {
+		this.mutantType = mutantType;
 	}
 
 }
